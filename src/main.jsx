940977bcd0bc7283ad1f90cb36f7fd513c80b274
chaikivskti.t <chaikivskyi.t@gmail.com>
2026-01-11 01:14:47 +0100

    ts -> js

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { store, persistor } from "@store/store";
 import App from "@/App";
 import "@/index.css";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
